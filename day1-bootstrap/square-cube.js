44eb9f1be00875fae0e6aef3d743c8cccef9d4b9
sinchana <sinchana1705@gmail.com>
2025-02-10 14:43:12 +0530

    organised into folder

--- day1-bootstrap/square-cube.js
+++ day1-bootstrap/square-cube.js
@@ -2,11 +2,11 @@ function doSquare() {
     let num = parseInt(document.getElementById("number").value);
     let square = num * num;
     document.getElementById("output").value = square;
-    document.getElementById("output_caption").innerHTML = "square";
+    document.getElementById("output_caption").innerHTML = "showing square";
 }
 function doCube() {
     let num = parseInt(document.getElementById("number").value);
     let cube = num * num * num;
     document.getElementById("output").value = cube;
-    document.getElementById("output_caption").innerHTML = "cube";
+    document.getElementById("output_caption").innerHTML = "showing cube";
 }
